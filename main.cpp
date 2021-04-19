02e05251571b876c75e85717dbcdc1f0421fef99
Valiev2002 <70635274+Valiev2002@users.noreply.github.com>
2021-04-20 00:38:48 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,103 +1,49 @@
 #include <iostream>
-#include <cmath>
+#include <stack>
+#include <sstream>
 using namespace std;
-int main()
-{
-    float a,b;
-    int z;
-    void Sine(float);
-    void Square(float);
-    void Cos(float);
-    void Tan(float);
-    void Log(float);
-    void Baselog(float);
-    cout<<"WHAT YOU WANT TO FIND: "<<endl;
-    cout<<"Press '1' for Sin: "<<endl;
-    cout<<"Press '2' for Square: "<<endl;
-    cout<<"Press '3' for Cos: "<<endl;
-    cout<<"Press '4' for Tan: "<<endl;
-    cout<<"Press '5' for Log: "<<endl;
-    cout<<"Press '6' for Base Log: "<<endl;
-    cin>>z;
-    switch(z)
+int main() {
+    std::string str;
+    std::getline(std::cin, str);
+    std::istringstream s(str);
+    stack<int> opz;
+    int value;
+    char sign;
+    while (!s.eof())
     {
-        case 1:
-            cout<<"Enter the Number for Calculating SIN: "<<endl;
-            cin>>a;
-            Sine(a);
-            break;
-
-        case 2:
-            cout<<"Enter the Number for Calculating Square: "<<endl;
-            cin>>a;
-            Square(a);
-            break;
-
-        case 3:
-            cout<<"Enter the Number for Calculating COS: "<<endl;
-            cin>>a;
-            Cos(a);
-            break;
-
-        case 4:
-            cout<<"Enter the Number for Calculating TAN: "<<endl;
-            cin>>a;
-            Tan(a);
-            break;
-
-        case 5:
-            cout<<"Enter the Number for Calculating LOG: "<<endl;
-            cin>>a;
-            Log(a);
-            break;
-
-        case 6:
-            cout<<"Enter the Number for Calculating LOG WITH BASE 10: "<<endl;
-            cin>>a;
-            Baselog(a);
-            break;
+        while (s >> value) {
+            opz.push(value);
+        }
+        if (!s.eof())
+        {
+            s.clear();
+            s.unget();
+            s >> sign;
+            if (opz.size() < 2)
+            {
+                cout << "Error"<< endl;
+                return 0;
+            }
+            int right = opz.top();
+            opz.pop();
+            int left = opz.top();
+            opz.pop();
+            switch (sign) {
+                case '+':
+                    opz.push(right + left);
+                    break;
+                case '-':
+                    opz.push(right - left);
+                    break;
+                case '*':
+                    opz.push(right * left);
+                    break;
+                case '/':
+                    opz.push(right / left);
+                    break;
+            }
+        }
     }
+    cout << "result value is " << opz.top()<<endl;
+    return 0;
 }
-
-void Sine(float x)
-{
-    float s;
-    s = sin(x);
-    cout<<"Sin: "<<s;
-}
-
-void Square(float x)
-{
-    float sq;
-    sq = sqrt(x);
-    cout<<"Square of a Given Value is: "<<sq;
-}
-
-void Cos(float x)
-{
-    float c;
-    c = cos(x);
-    cout<<"COS: "<<c;
-}
-
-void Tan(float x)
-{
-    float t;
-    t = tan(x);
-    cout<<"TAN: "<<t;
-}
-
-void Log(float x)
-{
-    float l;
-    l = log(x);
-    cout<<"Natural Logarithm: "<<l;
-}
-
-void Baselog(float x)
-{
-    float bl;
-    bl = log10(x);
-    cout<<"LOG with Base 10: "<<bl;
-}
-
